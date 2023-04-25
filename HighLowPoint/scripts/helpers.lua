337332bffa40bfd97ddba989d1b66fb1e48a61e9
GitLab Runner <gitlab-runner@ip-172-31-13-207.eu-central-1.compute.internal>
2023-04-25 18:39:26 +0000

    Update

--- HighLowPoint/scripts/helpers.lua
+++ HighLowPoint/scripts/helpers.lua
@@ -41,82 +41,6 @@ local function getTextDeco(rgba, size, xPos, yPos)
   return deco
 end
 
--- Get x coordinate
-local function getX(coordinate)
-  if type(coordinate) == 'userdata' then
-    return coordinate:getX()
-  else
-    return coordinate
-  end
-end
-
--- Make a profile into polylies
-local function profileToPolylines(
-  profile,
-  closed,
-  spaceBetweenPoints,
-  offsetX,
-  offsetY)
-  -- If there is no profile, return nothing
-  if not profile then
-    return {}
-  end
-
-  -- Set offset and closed
-  offsetX = offsetX or 0
-  offsetX = offsetX + getX(Profile.getCoordinate(profile, 0))
-  offsetY = offsetY or 0
-  closed = closed or false
-
-  -- Calculate the space between two profiles' points
-  if not spaceBetweenPoints then
-    if Profile.getSize(profile) > 1 then
-      spaceBetweenPoints =
-        getX(Profile.getCoordinate(profile, 1)) -
-        getX(Profile.getCoordinate(profile, 0))
-    else
-      spaceBetweenPoints = 1
-    end
-  end
-
-  -- Profile into vector
-  local values,
-    _,
-    validFlags = profile:toVector()
-
-  -- Create arrays
-  local polylines = {}
-  local pointBuff = {}
-
-  if Profile.getValidFlagsEnabled(profile) then
-    for i, value in pairs(values) do
-      value = value + offsetY
-
-      if validFlags[i] == 0 then
-        -- Commulate Buffer to polyline
-        if #pointBuff > 0 then
-          polylines[#polylines + 1] = Shape.createPolyline(pointBuff, closed)
-          pointBuff = {} -- Clear buffer
-        end
-      else
-        local x = offsetX + i * spaceBetweenPoints
-        pointBuff[#pointBuff + 1] = Point.create(x, value)
-      end
-    end
-  else -- Don't care about valid flags
-    for i, value in pairs(values) do
-      local x = offsetX + i * spaceBetweenPoints
-      pointBuff[#pointBuff + 1] = Point.create(x, value)
-    end
-  end
-
-  if #pointBuff > 0 then
-    polylines[#polylines + 1] = Shape.createPolyline(pointBuff, closed)
-  end
-
-  return polylines
-end
-
 -- Round integers
 local function round(n)
   return math.floor(n * 100) / 100
@@ -125,7 +49,6 @@ end
 local helper = {}
 helper.getDeco = getDeco
 helper.getTextDeco = getTextDeco
-helper.profileToPolylines = profileToPolylines
 helper.round = round
 
 return helper
